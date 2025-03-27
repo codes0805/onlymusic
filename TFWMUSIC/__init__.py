ba7ef2d3e2dbb991d5f261c36c1a841785985eb0
codes0805 <coderdeva99@gmail.com>
2025-03-27 23:32:29 +0530

    Add files via upload

---
+++ TFWMUSIC/__init__.py
@@ -0,0 +1,30 @@
+from TFWMUSIC.core.bot import Hotty
+from TFWMUSIC.core.dir import dirr
+from TFWMUSIC.core.git import git
+from TFWMUSIC.core.userbot import Userbot
+from TFWMUSIC.misc import dbb, heroku
+
+from SafoneAPI import SafoneAPI
+from .logging import LOGGER
+
+dirr()
+git()
+dbb()
+heroku()
+
+app = Hotty()
+userbot = Userbot()
+api = SafoneAPI()
+
+
+from .platforms import *
+
+Apple = AppleAPI()
+Carbon = CarbonAPI()
+SoundCloud = SoundAPI()
+Spotify = SpotifyAPI()
+Resso = RessoAPI()
+Telegram = TeleAPI()
+YouTube = YouTubeAPI()
+
+APP = "BRANDED_KUDI_BOT"  # connect music api key "Dont change it"
---
+++ TFWMUSIC/__main__.py
@@ -0,0 +1,62 @@
+import asyncio
+import importlib
+from sys import argv
+from pyrogram import idle
+from pytgcalls.exceptions import NoActiveGroupCall
+
+import config
+from TFWMUSIC import LOGGER, app, userbot
+from TFWMUSIC.core.call import Hotty
+from TFWMUSIC.misc import sudo
+from TFWMUSIC.plugins import ALL_MODULES
+from TFWMUSIC.utils.database import get_banned_users, get_gbanned
+from config import BANNED_USERS
+
+
+async def init():
+    if (
+        not config.STRING1
+        and not config.STRING2
+        and not config.STRING3
+        and not config.STRING4
+        and not config.STRING5
+    ):
+        LOGGER(__name__).error("Assistant client variables not defined, exiting...")
+        exit()
+    await sudo()
+    try:
+        users = await get_gbanned()
+        for user_id in users:
+            BANNED_USERS.add(user_id)
+        users = await get_banned_users()
+        for user_id in users:
+            BANNED_USERS.add(user_id)
+    except:
+        pass
+    await app.start()
+    for all_module in ALL_MODULES:
+        importlib.import_module("TFWMUSIC.plugins" + all_module)
+    LOGGER("TFWMUSIC.plugins").info("Successfully Imported Modules...")
+    await userbot.start()
+    await Hotty.start()
+    try:
+        await Hotty.stream_call("https://graph.org/file/e999c40cb700e7c684b75.mp4")
+    except NoActiveGroupCall:
+        LOGGER("TFWMUSIC").error(
+            "Please turn on the videochat of your log group\channel.\n\nStopping Bot..."
+        )
+        exit()
+    except:
+        pass
+    await Hotty.decorators()
+    LOGGER("TFWMUSIC").info(
+        "ᴅʀᴏᴘ ʏᴏᴜʀ ɢɪʀʟꜰʀɪᴇɴᴅ'ꜱ ɴᴜᴍʙᴇʀ ᴀᴛ @BRANDED_PAID_CC ᴊᴏɪɴ @TFW_BOTS_SUPPORT , @BRANDED_WORLD ꜰᴏʀ ᴀɴʏ ɪꜱꜱᴜᴇꜱ"
+    )
+    await idle()
+    await app.stop()
+    await userbot.stop()
+    LOGGER("TFWMUSIC").info("Stopping TFWMUSIC Bot...")
+
+
+if __name__ == "__main__":
+    asyncio.get_event_loop().run_until_complete(init())
