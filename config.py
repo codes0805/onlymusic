750b6b553df770857d80d63d1c6248ee09c36db5
codes0805 <coderdeva99@gmail.com>
2025-04-01 23:15:20 +0530

    Update config.py

--- config.py
+++ config.py
@@ -35,7 +35,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/Nishubots/TFW_ONLYMUSIC",
+    "https://github.com/codes0805/onlymusic",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv(
